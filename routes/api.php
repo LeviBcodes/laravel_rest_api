410027b356007b97659d2446221a3aa0c0ffdb0a
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2022-05-29 23:15:18 -0600

    adding get api routes for books and authors

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('authors', [App\Http\Controllers\AuthorController::class, 'index']);
+Route::get('books', [App\Http\Controllers\BookController::class, 'index']);
+
