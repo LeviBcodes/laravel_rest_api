bf083ee803a40fe62d40a4c8891dbb4d7b1197a5
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2022-05-29 22:05:43 -0600

    creating seed data using faker for books and authors

--- database/seeders/AuthorsTableSeeder.php
+++ database/seeders/AuthorsTableSeeder.php
@@ -2,11 +2,16 @@
 
 namespace Database\Seeders;
 
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Seeder;
+use App\Models\Author;
 
 class AuthorsTableSeeder extends Seeder
 {
+    
+    use HasFactory;
     /**
      * Run the database seeds.
      *
@@ -14,6 +19,9 @@ class AuthorsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //create 3 author instances using faker
+        Factory::create(Author::class, 3);
+        
+        
     }
 }
--- database/seeders/BooksTableSeeder.php
+++ database/seeders/BooksTableSeeder.php
@@ -4,9 +4,13 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class BooksTableSeeder extends Seeder
 {
+    use HasFactory;
     /**
      * Run the database seeds.
      *
@@ -14,6 +18,7 @@ class BooksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //create 10 books using faker
+        Factory::create(Book::class, 10);
     }
 }
