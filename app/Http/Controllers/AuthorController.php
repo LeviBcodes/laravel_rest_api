d0237bb7b631b511a104fe6a19b1b9e7cda01c2e
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2022-05-29 22:49:30 -0600

    creating Author and Books Controllers

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response(Author::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'title' => 'required|string',
+            'company' => 'required|string',
+            'email' => 'required|email|unique:authors',
+        ]);
+        
+        $author = new Author();
+        $author->name = $data['name'];
+        $author->title = $data['title'];
+        $author->company = $data['company'];
+        $author->email = $data['email'];
+        $author->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
